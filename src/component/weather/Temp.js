ef9c029c0de35bb8955a884341ff42c392541d73
lathiyaharmik <66663005+Lathiya50@users.noreply.github.com>
2021-09-13 14:59:01 +0530

    Update Temp.js

--- src/component/weather/Temp.js
+++ src/component/weather/Temp.js
@@ -8,7 +8,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=c66d49f9a78896808222124091b02036`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={Enter your API Key}`;
 
       let res = await fetch(url);
       let data = await res.json();
